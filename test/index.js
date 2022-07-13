1f140cbd84be5b984c1eeebda4efc05fd39f35a1
Issac Centeno <issac.centenoc@axity.com>
2022-07-13 02:13:22 -0500

    Update initial commit

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var expect = require('expect.js'),
-    tipMexico = require('..');
-
-describe('tip-mexico', function() {
-  it('should say hello', function(done) {
-    expect(tipMexico()).to.equal('Hello, world');
-    done();
-  });
-});
